d6acf20bb261e20252a31a20f3bfbc8ff227c842
Tomi Lukkarinen <lukkarinen.tj@gmail.com>
2016-12-16 22:51:38 +0200

    week 2, task 4 done

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,10 @@
 class Brewery < ActiveRecord::Base
   has_many :beers
+
+  def print_report
+    puts name
+    puts "established at year #{year}"
+    puts "numer of beers #{beers.count}"
+  end
+
 end
